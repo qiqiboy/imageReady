439822f2c6c2a5017b3e98c21c242d100bbefe18
qiqiboy <imqiqiboy@gmail.com>
2013-03-29 12:27:57 +0800

    添加

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 imageReady
 ==========
 
-检测图片加载完成的组件（图片延迟加载）
+> javascript图片预加载，监测图片加载完成，获取图片真实尺寸的组件（图片延迟加载）
+
+## 如何使用
+```javascript
+// 首先在页面中引入imageReady.js
+
+/**
+ * @param img string|Image 图片url或者Image对象
+ * @param onready Function 图片尺寸已获取（此时图片还未完全下载下来）
+ * @param onload Function 图片加载完成
+ * @param onerror Function 图片加载失败
+ *
+ * onready、onload、onerror的参数只有一个，都是Image对象
+ * imageReady('http://www.planeart.cn/demo/imgReady/vistas24.jpg',function(img){
+		console.log('图片已经ready了，可以获取宽高了')
+	},function(img){
+		console.log('图片已经下载完成了')
+	},function(img){
+		console.log('图片下载出错了')
+	})
+ */
+
+imageReady=function(img, onready, onload, onerror){}
+
+````
---
+++ imageReady.js
@@ -0,0 +1,57 @@
+/**
+ * imageReady v1.0.1
+ * By qiqiboy, http://www.qiqiboy.com, http://weibo.com/qiqiboy, 2013/03/29
+ */
+var imageReady=(function(){
+	var list=[],
+		timer=null,
+		prop=[['width','height'],['naturalWidth','naturalHeight']],
+		natural=Number(typeof document.createElement('img').naturalHeight==='number'),//是否支持HTML5新增的 naturalHeight
+		tick=function(){
+			for(var i=0;i<list.length;i++){
+				list[i].end?list.splice(i--,1):check.call(list[i],null);
+			}
+			list.length && (timer=setTimeout(tick,50)) || (timer=null);
+		},
+		/** overflow: 检测图片尺寸的改变
+		  *  img.__width,img.__height: 初载入时的尺寸
+		  */
+		check=function(){
+			if(this[prop[natural][0]]!==this.__width || this[prop[natural][1]]!==this.__height || this[prop[natural][0]]*this[prop[natural][1]]>1024){
+				this.onready(this);
+				this.end=true;
+			}
+		};
+		return function(_img, onready, onload, onerror){
+			onready=onready || new Function();
+			onload=onload || new Function();
+			onerror=onerror || new Function();
+			var img=typeof _img=='string'?new Image():_img;
+			img.onerror=function(){// ie && ie<=8 的浏览器必须在src赋予前定义onerror
+				onerror(img);
+				img.end=true;
+				img=img.onload=img.onerror=img.onreadystatechange=null;
+			}
+			if(typeof _img=='string') img.src=_img;
+			if(!img)return;
+			if(img.complete){
+				onready(img);
+				onload(img);
+				return;
+			}
+			img.__width=img[prop[natural][0]];
+			img.__height=img[prop[natural][1]];
+			img.onready=onready;
+			check.call(img,null);
+			img.onload=img.onreadystatechange=function(){
+				if(img&&img.readyState&&img.readyState!='loaded'&&img.readyState!='complete'){return;}
+				!img.end && check.call(img,null);
+				onload(img);
+				img=img.onload=img.onerror=img.onreadystatechange=null;
+			}
+			if(!img.end){
+				list.push(img);
+				if(timer===null) timer=setTimeout(tick,50);
+			}
+		}
+})();
