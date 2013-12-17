10de90c32c38b0567d9c87b89c0a6552b1d487ce
qiqiboy <imqiqiboy@gmail.com>
2013-12-17 12:07:06 +0800

    update

--- imageReady.js
+++ imageReady.js
@@ -6,10 +6,12 @@ var imageReady=(function(){
 	var list=[],
 		timer=null,
 		prop=[['width','height'],['naturalWidth','naturalHeight']],
-		natural=Number(typeof document.createElement('img').naturalHeight==='number'),//是否支持HTML5新增的 naturalHeight
+		natural=Number(typeof (new Image()).naturalWidth=='number'),//是否支持HTML5新增的 naturalHeight
 		tick=function(){
-			for(var i=0;i<list.length;i++){
-				list[i].end?list.splice(i--,1):check.call(list[i],null);
+			var i=0;
+			while(i<list.length){
+				list[i].end?list.splice(i--,1):check.call(list[i]);
+				i++;
 			}
 			list.length && (timer=setTimeout(tick,50)) || (timer=null);
 		},
@@ -22,36 +24,37 @@ var imageReady=(function(){
 				this.end=true;
 			}
 		};
-		return function(_img, onready, onload, onerror){
-			onready=onready || new Function();
-			onload=onload || new Function();
-			onerror=onerror || new Function();
-			var img=typeof _img=='string'?new Image():_img;
-			img.onerror=function(){// ie && ie<=8 的浏览器必须在src赋予前定义onerror
-				onerror(img);
-				img.end=true;
-				img=img.onload=img.onerror=img.onreadystatechange=null;
-			}
-			if(typeof _img=='string') img.src=_img;
-			if(!img)return;
-			if(img.complete){
-				onready(img);
-				onload(img);
-				return;
-			}
-			img.__width=img[prop[natural][0]];
-			img.__height=img[prop[natural][1]];
-			img.onready=onready;
-			check.call(img,null);
-			img.onload=img.onreadystatechange=function(){
-				if(img&&img.readyState&&img.readyState!='loaded'&&img.readyState!='complete'){return;}
-				!img.end && check.call(img,null);
-				onload(img);
-				img=img.onload=img.onerror=img.onreadystatechange=null;
-			}
-			if(!img.end){
-				list.push(img);
-				if(timer===null) timer=setTimeout(tick,50);
-			}
+		
+	return function(_img, onready, onload, onerror){
+		onready=onready || new Function();
+		onload=onload || new Function();
+		onerror=onerror || new Function();
+		var img=typeof _img=='string'?new Image():_img;
+		img.onerror=function(){// ie && ie<=8 的浏览器必须在src赋予前定义onerror
+			onerror.call(img,img);
+			img.end=true;
+			img=img.onload=img.onerror=img.onreadystatechange=null;
 		}
+		if(typeof _img=='string') img.src=_img;
+		if(!img)return; //为了防止onerror触发后img=null
+		if(img.complete){
+			onready.call(img,img);
+			onload.call(img,img);
+			return;
+		}
+		img.__width=img[prop[natural][0]];
+		img.__height=img[prop[natural][1]];
+		img.onready=onready;
+		check.call(img);
+		img.onload=img.onreadystatechange=function(){
+			if(img&&img.readyState&&img.readyState!='loaded'&&img.readyState!='complete'){return;}
+			!img.end && check.call(img);
+			onload.call(img,img);
+			img=img.onload=img.onerror=img.onreadystatechange=null;
+		}
+		if(!img.end){
+			list.push(img);
+			!timer && (timer=setTimeout(tick,50));
+		}
+	}
 })();
